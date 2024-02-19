7dc9c89468797a65d02445581270dc6bc0ace191
mrahman141 <mrahman141@myseneca.ca>
2024-02-18 21:46:12 -0500

    Reverted testing

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,7 @@
 export default function Home() {
   return (
     <>
-    {/* <h1>Hello World!</h1> */}
-    <h1>TESTING COMMITS DELETE THIS LATER</h1>
-    <h1>TEST 2</h1>
+    <h1>Hello World!</h1>
     </>
   );
 }
